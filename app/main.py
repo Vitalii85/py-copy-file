971f77e63f2e7a877420d21f36f2f0ea423bca63
Vitalii Kononets <vitalii.kononets.85@gmail.com>
2026-07-11 02:02:04 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,13 @@
-# write your code here
+def copy_file(command: str) -> None:
+    commands_list = command.split(" ")
+    if len(commands_list) < 3 or commands_list[0] != "cp":
+        return
+
+    try:
+        with (
+            open(commands_list[1], "r") as file_in,
+            open(commands_list[2], "w") as file_out
+        ):
+            file_out.write(file_in.read())
+    except FileNotFoundError:
+        return
